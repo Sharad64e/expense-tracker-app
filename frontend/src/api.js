1948845d5ce5c091b0d710bb3725335f3afc9947
Sharad Singh <you@example.com>
2026-03-22 20:52:43 +0530

    updated API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'https://expense-tracker-app-no71.onrender.com'
 
 const api = axios.create({
   baseURL: BASE_URL,
